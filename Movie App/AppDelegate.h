e92ae11fb27ec6259f93abf1b853b6e1d80b43dc
Dmitry Beloborodov <dmi3j@me.com>
2015-02-03 22:14:03 +0200

    Initial Commit

---
+++ Movie App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie App
+//
+//  Created by Dmitry Beloborodov on 03/02/15.
+//  Copyright (c) 2015 Mobile Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
